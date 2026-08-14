a39abf3d92fb2778a140626d7e32c22ddca27141
vtu29798 <vtu29798@veltech.edu.in>
2026-08-14 21:13:07 +0530

    Create Day of the year.java

---
+++ Week3/Day of the year.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int dayOfYear(String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8, 10));
+
+        int[] daysInMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+       
+        if (isLeapYear(year)) {
+            daysInMonth[2] = 29;
+        }
+
+        int dayOfYear = 0;
+        for (int i = 1; i < month; i++) {
+            dayOfYear += daysInMonth[i];
+        }
+        dayOfYear += day;
+
+        return dayOfYear;
+    }
+
+    private boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
