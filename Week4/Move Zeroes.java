1eb67e5f0c3daa20bb96d58aa648725586508787
vtu29798 <vtu29798@veltech.edu.in>
2026-09-01 09:56:25 +0530

    Update and rename Move Zeroes.java to Move Zeroes.java

--- Week4/Move Zeroes.java
+++ Week4/Move Zeroes.java
@@ -2,7 +2,7 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int index = 0;
 
-        // Move all non-zero elements to the front
+
         for (int num : nums) {
             if (num != 0) {
                 nums[index] = num;
@@ -10,7 +10,7 @@ class Solution {
             }
         }
 
-        // Fill remaining positions with zero
+
         while (index < nums.length) {
             nums[index] = 0;
             index++;
