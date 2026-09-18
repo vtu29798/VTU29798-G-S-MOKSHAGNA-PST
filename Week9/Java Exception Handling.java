67e6edbe62b2459f962371af730a2d2cdcea62af
vtu29798 <vtu29798@veltech.edu.in>
2026-09-18 11:56:50 +0530

    Create Java Exception Handling.java

---
+++ Week9/Java Exception Handling.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            System.out.println(x / y);
+        } 
+        catch (ArithmeticException e) {
+            System.out.println("java.lang.ArithmeticException: / by zero");
+        } 
+        catch (InputMismatchException e) {
+            System.out.println("java.util.InputMismatchException");
+        }
+
+        sc.close();
+    }
+}
